ece689c8edc50462dd28e24fa8a760276028d533
jason oboti <obotijason@gmail.com>
2022-07-02 23:25:13 +0100

    M

--- js/main.js
+++ js/main.js
@@ -25,7 +25,32 @@
         }
     });
     
-    
+    // smooth scrolling
+    var scrollLink = $('.scroll')
+
+    scrollLink.click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000)
+    });
+
+    //active link switch
+    $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function(){
+
+            var sectionOffset = $(this.hash).offset().top
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active')
+            }
+        })
+    })
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
